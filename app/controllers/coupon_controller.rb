7209446e202f55a2c20f8442da6191d165f87fde
Chance Smith <chancesmith97@hotmail.com>
2020-07-01 21:02:59 +0000

    Automatically backed up by Learn

--- app/controllers/coupon_controller.rb
+++ app/controllers/coupon_controller.rb
@@ -1,19 +1,19 @@
 class CouponController < ApplicationController
 
-  def index 
-    @coupons = Coupon.all 
+  def index
+    @coupons = Coupon.all
   end
 
-  def show 
+  def show
     @coupon = Coupon.find(params[:id])
   end
 
-  def new 
-    @coupon = Coupon.new 
+  def new
+    @coupon = Coupon.new
   end
 
-  def create 
-    @coupon = Coupon.new 
+  def create
+    @coupon = Coupon.new
     @coupon.coupon_code = params[:coupon_code]
     @coupon.store = params[:store]
     @coupon.save
