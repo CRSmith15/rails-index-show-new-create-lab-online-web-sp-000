1cd66e8b1c88eeebfc7afce9424e105a71419c57
Chance Smith <chancesmith97@hotmail.com>
2020-07-01 22:05:44 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -13,8 +13,8 @@
 ActiveRecord::Schema.define(version: 1) do
 
   create_table "coupons", force: :cascade do |t|
-    t.string "coupon_code"
-    t.string "store"
+    t.string "coupon[coupon_code]"
+    t.string "coupon[store]"
   end
 
 end
